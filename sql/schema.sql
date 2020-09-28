dec7f42d097e78a805b34900ff7456c1450dd96b
Aaron Slusser <aaron.slusser1@gmail.com>
2020-09-28 18:38:22 -0400

    fixed sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,5 +3,48 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE users(
   username VARCHAR(20) NOT NULL,
   fullname VARCHAR(40) NOT NULL,
+  email VARCHAR(40) NOT NULL,
+  filename VARCHAR(64) NOT NULL,
+  password VARCHAR(256) NOT NULL,
+  created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY(username)
 );
+
+CREATE TABLE posts(
+  postid INT NOT NULL,
+  filename VARCHAR(64) NOT NULL,
+  owner VARCHAR(20) NOT NULL,
+  created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(postid),
+  FOREIGN KEY (owner) REFERENCES users(username) ON DELETE CASCADE
+);
+
+CREATE TABLE following(
+  username1 VARCHAR(20) NOT NULL,
+  username2 VARCHAR(20) NOT NULL,
+  created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(username1, username2),
+  FOREIGN KEY (username1) REFERENCES users(username) ON DELETE CASCADE,
+  FOREIGN KEY (username2) REFERENCES users(username) ON DELETE CASCADE
+);
+
+CREATE TABLE comments(
+  commentid INT NOT NULL,
+  owner VARCHAR(20) NOT NULL,
+  postid VARCHAR(20) NOT NULL,
+  text TEXT(1024) NOT NULL,
+  created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(commentid),
+  FOREIGN KEY (owner) REFERENCES users(username) ON DELETE CASCADE,
+  FOREIGN KEY (postid) REFERENCES posts(postid) ON DELETE CASCADE
+);
+
+CREATE TABLE likes(
+  owner VARCHAR(20) NOT NULL,
+  postid VARCHAR(20) NOT NULL,
+  PRIMARY KEY(owner,post_id),
+  FOREIGN KEY (owner) REFERENCES users(username) ON DELETE CASCADE,
+  FOREIGN KEY (postid) REFERENCES posts(postid) ON DELETE CASCADE
+);
+
+
