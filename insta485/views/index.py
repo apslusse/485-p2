e6eca0558762e49a233b3030e2bdab4e6e43e488
Aaron Slusser <aaron.slusser1@gmail.com>
2020-09-28 19:30:09 -0400

    fixed insta485

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -11,5 +11,17 @@ import insta485
 @insta485.app.route('/')
 def show_index():
     """Display / route."""
-    context = {}
+
+    # Connect to database
+    connection = insta485.model.get_db()
+
+    # Query database
+    cur = connection.execute(
+        "SELECT username, fullname "
+        "FROM users"
+    )
+    users = cur.fetchall()
+
+    # Add database info to context
+    context = {"users": users}
     return flask.render_template("index.html", **context)
